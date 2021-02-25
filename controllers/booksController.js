8d7627869980d59d2b63f2f577d663293296b97b
wutango <nickkdb@gmail.com>
2021-02-25 13:59:05 -0500

    removing catch functions from express

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -6,18 +6,19 @@ module.exports = {
     getAll: (req, res) => {
         Book.find({}).then(data => {
             res.json(data)
-        }).catch(err => console.error(err));
+        })
+        // .catch(err => console.error(err));
     },
     insertBook: (req, res) => {
         Book.create(req.body)
         .then(data => res.json(data))
-        .catch(err => console.error(err));
+        // .catch(err => console.error(err));
     },
     deleteBook: (req, res) => {
         Book.remove({
             id: req.params.id
         })
         .then(data => res.json(data))
-        .catch(err => console.error(err));
+        // .catch(err => console.error(err));
     }
 }
