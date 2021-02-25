6f68ef4e8bed4d26831abf7e4d8d0226f5d041c0
wutango <nickkdb@gmail.com>
2021-02-25 14:04:39 -0500

    bug fixes

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -7,18 +7,18 @@ module.exports = {
         Book.find({}).then(data => {
             res.json(data)
         })
-        // .catch(err => console.error(err));
+        .catch(err => {res.status(422).json(err)})
     },
     insertBook: (req, res) => {
         Book.create(req.body)
         .then(data => res.json(data))
-        // .catch(err => console.error(err));
+        .catch(err => {res.status(422).json(err)})
     },
     deleteBook: (req, res) => {
         Book.remove({
             id: req.params.id
         })
         .then(data => res.json(data))
-        // .catch(err => console.error(err));
+        .catch(err => {res.status(422).json(err)})
     }
 }
